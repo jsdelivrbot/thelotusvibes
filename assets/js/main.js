e50a79de673e27c3b13e5f3ce9b7c3493f94cc0e
Ada Chiu <adachiu06@gmail.com>
2017-03-25 00:56:10 -0700

    edit instafeed filter

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,9 @@ var userFeed = new Instafeed({
     userId: '4078788353',
     accessToken: '4078788353.1677ed0.4112454355f04fcf88f3db2939f3cf27',
     resolution: 'standard_resolution',
-    limit: 6,
+    limit: 7,
     template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /></a>',
+    // filter #lotusvibes365 for designs
     filter: function(image) {
     return image.tags.indexOf('lotusvibes365') >= 0;
     }
